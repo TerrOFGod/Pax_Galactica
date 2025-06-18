840136ad8787726a7785605255e0913dea925f34
HihiHaha <efwfw.ffew@mail.ru>
2025-06-18 22:50:00 +0300

    Add fog and add fix scene

--- Pax_Galactica/Assets/Scripts/UI/BuildingPanel.cs
+++ Pax_Galactica/Assets/Scripts/UI/BuildingPanel.cs
@@ -1,28 +1,29 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
 public class BuildingPanel : MonoBehaviour
 {
+    [SerializeField]
+    private string pathProfiles = "Buildings/";
     public GameObject BuildingButtonTemplate;
     [SerializeField]
     private Transform _buttonParent;
     private List<BuildingProfile> _buildings;
+
+    private List<BuildingPresentOnButton> buildingButtons = new List<BuildingPresentOnButton>();
+
     void Start()
     {
-        _buildings = Resources.LoadAll<BuildingProfile>("Buildings/").ToList();// получение листа зданий и создание кнопок
+        _buildings = Resources.LoadAll<BuildingProfile>(pathProfiles).ToList();// получение листа зданий и создание кнопок
 
         foreach (var building in _buildings)
         {
             var buttonGo = Instantiate(BuildingButtonTemplate, _buttonParent);
-            buttonGo.GetComponent<BuildingPresentOnButton>().Present(building);
+            var buildingButton = buttonGo.GetComponent<BuildingPresentOnButton>();
+            buildingButtons.Add(buildingButton);
+            buildingButton.Present(building);
+            //buildingButton.gameObject.SetActive(false);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
